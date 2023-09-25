5d5b0988e147bab8d94328a1ce9003edd90f52e1
Roger Xu <roger.xu@smoothstack.com>
2023-09-25 11:52:08 -0700

    finished setting up

---
+++ src/main/java/com/cooksys/team1assess1/Team1assess1Application.java
@@ -0,0 +1,13 @@
+package com.cooksys.team1assess1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1assess1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1assess1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/team1assess1/Team1assess1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.team1assess1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1assess1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
