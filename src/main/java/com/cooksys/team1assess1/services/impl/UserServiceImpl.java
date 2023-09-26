feb24d1370d91598e14757bea07f3c3ce0985603
smalhadid <smalhadid@gmail.com>
2023-09-26 09:01:08 -0400

    fixed user service issue

--- src/main/java/com/cooksys/team1assess1/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/team1assess1/services/impl/UserServiceImpl.java
@@ -1,9 +1,10 @@
 package com.cooksys.team1assess1.services.impl;
 
+import com.cooksys.team1assess1.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 }
