8a4a57b345a88d4822e96c424046b306746f2ce3
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-09-27 11:25:41 -0500

    changed field types

--- src/main/java/com/cooksys/team1assess1/dtos/UserRequestDto.java
+++ src/main/java/com/cooksys/team1assess1/dtos/UserRequestDto.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserRequestDto {
-    private Credentials credentials;
-    private Profile profile;
+    private CredentialsDto credentials;
+    private ProfileDto profile;
 }
