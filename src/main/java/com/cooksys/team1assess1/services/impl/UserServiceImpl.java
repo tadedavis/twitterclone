785f91cc3a209ff23da960c08b9e5f8e28af57be
smalhadid <smalhadid@gmail.com>
2023-09-28 16:09:40 -0400

    trying to fix failures

--- src/main/java/com/cooksys/team1assess1/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/team1assess1/services/impl/UserServiceImpl.java
@@ -97,7 +97,7 @@ public class UserServiceImpl implements UserService {
 		User updatedUser = userMapper.dtoToEntity(userRequestDto);
 
 
-		if (userToUpdate.getProfile() == null) {
+		if (updatedUser.getProfile() == null) {
 			throw new BadRequestException("User has no profile");
 		}
 
